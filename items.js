ecf27c98457df0c243319a868dcf1c1bc5ef7228
kevmill29 <kev29@fanaticalkev.com>
2025-11-12 08:26:59 -0500

    Fixing project structure

--- items.js
+++
@@ -1,3 +0,0 @@
-fetch("url that we are calling")
-    .then(response => response.json()) //first fetch then response lambda response.json to turn into a javascript file
-    .then(data => console.log(data)) //then after access the data that we are getting from the api
